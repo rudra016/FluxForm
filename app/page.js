158ed9d832b4b80565da168a406205390803d6bf
Rudra Sharma <94120092+rudra016@users.noreply.github.com>
2024-08-09 14:44:29 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,8 +3,15 @@ import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-   <div>
-    <Hero />
-   </div>
+  
+   <>
+      <Head>
+        <title>FluxForm AI</title>
+        <meta name="description" content="FluxForm is an AI-powered platform built with Next.js, DaisyUI, HyperUI, Drizzle ORM, Neon Database, and Clerk, designed to simplify form, survey, and questionnaire creation. Experience an intuitive and efficient form-building process with FluxForm." />
+      </Head>
+       <div>
+        <Hero />
+       </div>
+    </>
   );
 }
