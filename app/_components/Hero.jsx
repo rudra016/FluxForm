f9043e20ef2da448e2be1b46f3b1501cfbfde2a3
Rudra Sharma <rudra619kumar@gmail.com>
2025-07-03 22:54:27 +0530

    Update Hero.jsx

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -48,14 +48,7 @@ const Hero = () => {
   </div>
 </section>
 
-      <div className="fixed inset-x-0 bottom-0 p-4">
-    <div className="rounded-lg bg-primary px-4 py-3 text-white shadow-lg">
-      <p className="text-center text-sm font-medium">
-        Made with 💖 by <span><a href="https://github.com/rudra016" className="inline-block underline"> Rudra </a></span> 
-        
-      </p>
-    </div>
-  </div>
+      
     </div>
   )
 }
